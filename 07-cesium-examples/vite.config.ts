1bd12df07a55c0dad04d23dce570e913ba56b5a5
lanjinjun <lanjj995@163.com>
2025-01-15 09:08:05 +0800

    feat(07-cesium-examples): ...

---
+++ 07-cesium-examples/vite.config.ts
@@ -0,0 +1,17 @@
+import { ConfigEnv, defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import cesium from 'vite-plugin-cesium'
+
+export default defineConfig((configEnv: ConfigEnv) => {
+  
+  const env = loadEnv(configEnv.mode, process.cwd(), '')
+  
+  return {
+    plugins: [
+      vue(),
+      cesium({
+        cesiumBaseUrl: env.VITE_CESIUM_BASE_URL
+      })
+    ]
+  }
+})
